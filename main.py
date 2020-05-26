6aa809d247fcee21294592302f57ba4d559a2495
jeffreyt <jeff@ibisnetworks.com>
2020-05-25 16:28:22 -1000

    guy facing around

--- main.py
+++ main.py
@@ -3,33 +3,36 @@ import random
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
-NUMBER_OF_BALLS = 3
-SCREEN_TITLE = "punky brew"
+NUMBER_OF_BALLS = 10
+MOVEMENT_SPEED = 3
+SCREEN_TITLE = "me hungry"
 
 
 class Pakumon:
-    def __init__(self, x, y, dx, dy, r, dr):
-        self.x = x
-        self.y = y
-        self.dx = dx
-        self.dy = dy
-        self.r = r
-        self.dr = dr
+    def __init__(self, pos_x, pos_y, mouth_speed, radius):
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.delta_x = 0
+        self.delta_y = 0
+        self.mouth = 0
+        self.delta_mouth = mouth_speed
+        self.radius = radius
+        self.orientation = 0
 
     def draw(self):
         # arcade.draw_circle_filled(self.x, self.y, 50, arcade.color.BANANA_YELLOW)
-        mouth_top = 30 - self.r
-        mouth_bottom = 330 + self.r
-        arcade.draw_arc_filled(self.x, self.y, 50, 50, arcade.color.BANANA_YELLOW, mouth_top, mouth_bottom)
-        arcade.draw_arc_outline(self.x, self.y, 50, 50, arcade.color.BLACK, mouth_top, mouth_bottom)
-        arcade.draw_circle_filled(self.x+4, self.y+10, 2.5, arcade.color.BLACK)
+        mouth_top = self.orientation + 30 - self.mouth
+        mouth_bottom = self.orientation + 330 + self.mouth
+        arcade.draw_arc_filled(self.pos_x, self.pos_y, self.radius, self.radius, arcade.color.BANANA_YELLOW, mouth_top, mouth_bottom)
+        # arcade.draw_arc_outline(self.x, self.y, 50, 50, arcade.color.BLACK, mouth_top, mouth_bottom)
 
     def update(self):
-        self.r += self.dr
-        if self.r > 30 or self.r < 0:
-            self.dr *= -1
+        self.mouth += self.delta_mouth
+        if self.mouth > 30 or self.mouth < 0:
+            self.delta_mouth *= -1
 
-        return
+        self.pos_x = clamp(self.pos_x + self.delta_x, 0 + self.radius, SCREEN_WIDTH - self.radius)
+        self.pos_y = clamp(self.pos_y + self.delta_y, 0 + self.radius, SCREEN_HEIGHT - self.radius)
 
 
 class BouncyBall:
@@ -44,7 +47,7 @@ class BouncyBall:
 
     def draw(self):
         arcade.draw_circle_filled(self.pos_x, self.pos_y, self.radius, self.color)
-        arcade.draw_circle_outline(self.pos_x, self.pos_y, self.radius, arcade.color.CYBER_YELLOW)
+        # arcade.draw_circle_outline(self.pos_x, self.pos_y, self.radius, arcade.color.CYBER_YELLOW)
 
     def update(self):
         self.pos_x += self.delta_x
@@ -57,15 +60,18 @@ class BouncyBall:
             self.delta_y *= -1
 
 
-class FeedingFrenzy(arcade.Window):
+class HungryCircleGuy(arcade.Window):
 
     def __init__(self, width, height, title):
+        # setup window
         super().__init__(width, height, title)
         self.set_mouse_visible(False)
         arcade.set_background_color(arcade.color.COOL_GREY)
-        self.balls = []
 
-        self.p1 = Pakumon(SCREEN_WIDTH*0.5, SCREEN_HEIGHT*0.5, 0, 0, 0, 1)
+        # construct
+        self.balls = []
+        self.score = 0
+        self.p1 = Pakumon(SCREEN_WIDTH*0.5, SCREEN_HEIGHT*0.5, 1, 50)
         for x in range(NUMBER_OF_BALLS):
             self.balls.append(BouncyBall(*ball_randomizer(radius=10, color=arcade.color.GHOST_WHITE)))
 
@@ -75,14 +81,38 @@ class FeedingFrenzy(arcade.Window):
         for ball in self.balls:
             ball.draw()
 
+        # text hud
+        output = f"Score: {self.score}"
+        arcade.draw_text(output, 10, 20, arcade.color.RED, 18)
+
     def update(self, delta_time):
         self.p1.update()
         for ball in self.balls:
             ball.update()
 
-    def on_mouse_motion(self, x, y, dx, dy):
-        self.p1.x = x
-        self.p1.y = y
+    def on_key_press(self, key, modifiers):
+        if key == arcade.key.LEFT:
+            self.p1.orientation = 180
+            self.p1.delta_x = -MOVEMENT_SPEED
+        elif key == arcade.key.RIGHT:
+            self.p1.orientation = 0
+            self.p1.delta_x = MOVEMENT_SPEED
+        elif key == arcade.key.UP:
+            self.p1.orientation = 90
+            self.p1.delta_y = MOVEMENT_SPEED
+        elif key == arcade.key.DOWN:
+            self.p1.orientation = 270
+            self.p1.delta_y = -MOVEMENT_SPEED
+
+    def on_key_release(self, key, modifiers):
+        if key == arcade.key.LEFT or key == arcade.key.RIGHT:
+            self.p1.delta_x = 0
+        elif key == arcade.key.UP or key == arcade.key.DOWN:
+            self.p1.delta_y = 0
+
+    # def on_mouse_motion(self, x, y, dx, dy):
+    #     self.p1.x = x
+    #     self.p1.y = y
 
 def ball_randomizer(radius=None, color=None):
 
@@ -107,7 +137,7 @@ def clamp(value, minval, maxval):
 
 def main():
     # setup window
-    window = FeedingFrenzy(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    window = HungryCircleGuy(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
 
     # display
     arcade.run()
